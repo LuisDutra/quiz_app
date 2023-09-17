93d08f640d8c581f80c3615248fb09a0be54b33d
Luis Vinicius <luisvinicius99@gmail.com>
2023-09-17 16:50:51 -0300

    center butons and text

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -16,21 +16,27 @@ class _QuestionsScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
     final currentQuestion = questions[0];
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text(
-          currentQuestion.text,
-          style: const TextStyle(color: Colors.white),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
-        AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
-        AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
-        AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
-      ],
+    return Container(
+      margin: const EdgeInsets.all(40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          })
+        ],
+      ),
     );
   }
 }
